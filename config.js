6ecf235b88bf8457f748c03e35332068db6da656
fromhell13 <zalizan13@gmail.com>
2016-05-06 18:05:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
 
-    'secret': 'projekblissvotecontestnestle2016',
-    'database': 'mongodb://bliss:bl!$$+2016@ec2-52-76-151-250.ap-southeast-1.compute.amazonaws.com:27017/projekbliss'
+    'secret': 'xxxxx',
+    'database': 'mongodb://xxxxxx'
 
 };
